3c3353a537c66e3d321d2437480c3acb83c4f789
Stijn van der Ree <stijn@laposta.nl>
2016-06-06 22:32:52 +0200

    Updated create field example to include options

--- examples/field/create.php
+++ examples/field/create.php
@@ -3,15 +3,17 @@ require_once('../../lib/Laposta.php');
 Laposta::setApiKey("JdMtbsMq2jqJdQZD9AHC");
 
 // initialize field with list_id
-$field = new Laposta_Field("BaImMu3JZA");
+$field = new Laposta_Field("srbhotdwob");
 
 try {
 	// create new field, insert info as argument
 	// $result will contain een array with the response from the server
 	// Note: we need to put 'true' in quotes, because php translates true to 1 and false to [empty]
 	$result = $field->create(array(
-		'name' => 'Naam',
-		'datatype' => 'text',
+		'name' => 'Kleur',
+		'defaultvalue' => 'Groen',
+		'datatype' => 'select_multiple',
+		'options' => array('Rood', 'Groen', 'Blauw'),
 		'required' => 'true',
 		'in_form' => 'true',
 		'in_list' => 'true'
@@ -25,29 +27,4 @@ try {
 	// you can use the information in $e to react to the exception
 	print '<pre>';print_r($e);print '</pre>';
 }
-
-// example with select_multiple options
-/*
-try {
-	// create new field, insert info as argument
-	// $result will contain een array with the response from the server
-	// Note: we need to put 'true' in quotes, because php translates true to 1 and false to [empty]
-	$result = $field->create(array(
-		'name' => 'Favoriete kleuren',
-		'datatype' => 'select_multiple',
-		'required' => 'false',
-		'in_form' => 'false',
-		'in_list' => 'false',
-		'options' => array('Rood', 'Groen', 'Blauw')
-		)
-	);
-
-	print '<pre>';print_r($result);print '</pre>';
-
-} catch (Exception $e) {
-
-	// you can use the information in $e to react to the exception
-	print '<pre>';print_r($e);print '</pre>';
-}
-*/
 ?>
